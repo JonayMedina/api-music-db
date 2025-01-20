d154879786dd2cd5e4b26ffec2ada9f5be500117
Jonay Medina <jmedina@tribalworldwide.gt>
2025-01-20 13:23:03 -0400

    :hammer: added fixes on song structs

--- database/structs/songs.go
+++ database/structs/songs.go
@@ -1,20 +1,21 @@
 package structs
 
 type Song struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	ArtistID    int    `json:"artist_id"`
-	Duration    int    `json:"duration"`
-	Album       string `json:"album"`
-	Genre       string `json:"genre"`
-	ReleaseDate string `json:"release_date"`
-	CoverImage  string `json:"cover_image"`
-	CreatedAt   string `json:"created_at"`
-	PlaylistID  int    `json:"playlist_id,omitempty"`
+	ID          int     `json:"id,omitempty"`
+	Title       string  `json:"title,omitempty"`
+	ArtistID    int     `json:"artist_id,omitempty"`
+	Duration    int     `json:"duration,omitempty"`
+	Album       string  `json:"album,omitempty"`
+	Genre       string  `json:"genre,omitempty"`
+	ReleaseDate string  `json:"release_date,omitempty"`
+	CoverImage  string  `json:"cover_image,omitempty"`
+	CreatedAt   string  `json:"created_at,omitempty"`
+	PlaylistID  int     `json:"playlist_id,omitempty"`
+	Artist      *Artist `json:"artist,omitempty"`
 }
 
 type Artist struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	CreatedAt string `json:"created_at"`
+	ID        int    `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	CreatedAt string `json:"created_at,omitempty"`
 }
