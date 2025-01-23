d166ac6ccee174100a84cde87021dac7e62f1c85
Jonay Medina <jmedina@tribalworldwide.gt>
2025-01-23 12:30:35 -0400

    :sparkles: added password encryption

--- mysql/db_func_users.go
+++ mysql/db_func_users.go
@@ -11,6 +11,38 @@ func (db DBServer) GetUser(userID int) (*structs.User, error) {
 	return getUser(db.DB, userID)
 }
 
+func (db DBServer) GetUsers() ([]*structs.User, error) {
+	return getUsers(db.DB)
+}
+
+func (db DBServer) CreateUser(user *structs.User) (*structs.User, error) {
+	return createUser(db.DB, user)
+}
+
+func getUsers(db *sql.DB) ([]*structs.User, error) {
+	users := []*structs.User{}
+
+	rows, err := db.Query(`SELECT * FROM users`)
+	if err != nil {
+		log.Println("error al obtener los usuarios", err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		user := &structs.User{}
+		err := rows.Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.CreatedAt)
+		if err != nil {
+			log.Println("error al obtener el usuario", err)
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
 func getUser(db *sql.DB, userID int) (*structs.User, error) {
 	user := &structs.User{}
 
@@ -42,6 +74,18 @@ func getUser(db *sql.DB, userID int) (*structs.User, error) {
 			log.Println("error al obtener el usuario", err)
 			return nil, err
 		}
+	}
+	return user, nil
+}
+
+func createUser(db *sql.DB, user *structs.User) (*structs.User, error) {
+	query := `INSERT INTO users (username, email, password, created_at) VALUES (?, ?, ?, ?)`
+	user.HashPassword()
+
+	_, err := db.Exec(query, user.Username, user.Email, user.Password, getNowDateTime())
+	if err != nil {
+		log.Println("error al crear el usuario", err)
+		return nil, err
 	}
 	return user, nil
 }
